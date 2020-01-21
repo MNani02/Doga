99558fe4086846de9873edd2005bcd9b810ab8ea
MNani02 <60153367+MNani02@users.noreply.github.com>
2020-01-21 20:44:40 +0100

    Add files via upload

---
+++ Buborék.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doga_MD
+{
+    public class Rand 
+    {
+        public static int randnum;
+        public Rand()
+        {
+            Random R = new Random();
+            randnum = R.Next(-101, 101);
+        }
+    }
+    public class Buborék : Program
+    {
+        public static void Rendezés()
+        {
+
+            for (int j = n - 1; j > 0; j--)
+            {
+                for (int k = 0; k < j; k++)
+                {
+                    if (tömb[k] > tömb[k + 1])
+                    {
+                        int temp = tömb[k + 1];
+                        tömb[k + 1] = tömb[k];
+                        tömb[k] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Doga_MD
+{
+    public class Program
+    {
+        public static int[] tömb = new int[10];
+        public static int n = tömb.Length;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                _ = new Rand();
+                tömb[i] = Rand.randnum;
+            }
+            Console.Write("Rendezetlen: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", tömb[i]);
+            }
+            Console.WriteLine();
+            Buborék.Rendezés();
+            Console.Write("Rendezve: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", tömb[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
